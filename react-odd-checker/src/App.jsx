807f119060693960b2836248cfe960b4f2f01b8f
Seungwon Lee <sukam09@naver.com>
2026-01-22 23:26:47 +0900

    feat: odd checker 리액트로 구현

---
+++ react-odd-checker/src/App.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+
+// API 로직
+function checkOdd(number) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (number % 2 === 1) {
+        resolve(number);
+      } else {
+        reject(number);
+      }
+    }, 500);
+  });
+}
+
+const inputValues = [1, 2, 3, 4, 5];
+
+export default function App() {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const hasError = errorMessage !== '';
+
+  useEffect(() => {
+    async function fetchResults(values) {
+      const promises = values.map(value => checkOdd(value));
+      const results = await Promise.allSettled(promises);
+      return results.map((result, index) => {
+        const isOdd = result.status === 'fulfilled';
+        const number = isOdd ? result.value : result.reason;
+        const message = isOdd ? '성공' : '실패';
+        return { id: index + 1, text: `${number}: ${message}`, isOdd };
+      });
+    }
+
+    fetchResults(inputValues)
+      .then(results => {
+        setResults(results);
+      })
+      .catch(error => {
+        setErrorMessage(error.message || '알 수 없는 에러가 발생했습니다.');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return <>{isLoading ? <Loading /> : hasError ? <Error errorMessage={errorMessage} /> : <List results={results} />}</>;
+}
+
+function Loading() {
+  return <h1>로딩 중...</h1>;
+}
+
+function Error({ errorMessage }) {
+  return <p style={{ color: 'orange' }}>{errorMessage}</p>;
+}
+
+function List({ results }) {
+  // 함수 인자를 result? result.isOdd?
+  const getResultColor = isOdd => (isOdd ? 'green' : 'red');
+
+  return (
+    <ul>
+      {results.map(result => (
+        <li key={result.id} style={{ color: `${getResultColor(result.isOdd)}` }}>
+          {result.text}
+        </li>
+      ))}
+    </ul>
+  );
+}
